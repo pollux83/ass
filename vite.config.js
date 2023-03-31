8092009748f0318bb5d4fcb990a56149a1aa5500
pollux83 <pollux83@tut.by>
2023-03-31 21:15:13 +0300

    vite config dev fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        hmr: {
+            host: '0.0.0.0',
+            port: 5173,
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
