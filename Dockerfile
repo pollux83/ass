e21a2a90717b8c1ae9b731b2021b21e3c242d0da
pollux83 <pollux83@tut.by>
2023-04-07 17:34:24 +0300

    install dusk

--- Dockerfile
+++ Dockerfile
@@ -8,14 +8,12 @@ RUN apt-get update && \
 RUN docker-php-ext-install pdo pdo_pgsql gd xml zip
 
 
-#Install Google Chrome
-#RUN apt-get -y install libxpm4 libxrender1 libgtk2.0-0 libnss3 libgconf-2-4 gnupg
-#RUN apt-get -y install zip unzip
-
+# Install Google Chrome
 RUN curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 RUN echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
 RUN apt-get -y update
 RUN apt-get -y install google-chrome-stable
+
 
 # Install Composer
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
@@ -27,16 +25,16 @@ RUN curl -sL https://deb.nodesource.com/setup_14.x | bash - && \
     apt-get install -y nodejs
 
 # Copy project files
-#COPY . /var/www/html
+COPY . /var/www/html
 
 # Install project dependencies
 #RUN composer install --no-interaction --optimize-autoloader --no-suggest
 
 # Set permissions
-#RUN chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+RUN chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 
 # Expose port 8889
-#EXPOSE 8889
+EXPOSE 8889
 
 # Start the server
 #CMD php artisan serve --host=0.0.0.0 --port=8889
--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -2,8 +2,9 @@
 
 namespace Tests;
 
-use Illuminate\Support\Collection;
 use Facebook\WebDriver\Chrome\ChromeOptions;
+use Illuminate\Support\Collection;
+use Facebook\WebDriver\ChromeChromeOptions;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Laravel\Dusk\TestCase as BaseTestCase;
@@ -12,55 +13,98 @@ abstract class DuskTestCase extends BaseTestCase
 {
     use CreatesApplication;
 
+//    /**
+//     * Prepare for Dusk test execution.
+//     *
+//     * @beforeClass
+//     */
+//    public static function prepare(): void
+//    {
+//        if (! static::runningInSail()) {
+//            static::startChromeDriver();
+//        }
+//    }
+//
+//    /**
+//     * Create the RemoteWebDriver instance.
+//     */
+//    protected function driver(): RemoteWebDriver
+//    {
+//        $options = (new ChromeOptions)->addArguments(collect([
+//            $this->shouldStartMaximized() ? '--start-maximized' : '--window-size=1920,1080',
+//        ])->unless($this->hasHeadlessDisabled(), function (Collection $items) {
+//            return $items->merge([
+//                '--disable-gpu',
+//                '--headless=new',
+//            ]);
+//        })->all());
+//
+//        return RemoteWebDriver::create(
+//            $_ENV['DUSK_DRIVER_URL'] ?? 'http://localhost:9515',
+//            DesiredCapabilities::chrome()->setCapability(
+//                ChromeOptions::CAPABILITY, $options
+//            )
+//        );
+//    }
+//
+//    /**
+//     * Determine whether the Dusk command has disabled headless mode.
+//     */
+//    protected function hasHeadlessDisabled(): bool
+//    {
+//        return isset($_SERVER['DUSK_HEADLESS_DISABLED']) ||
+//               isset($_ENV['DUSK_HEADLESS_DISABLED']);
+//    }
+//
+//    /**
+//     * Determine if the browser window should start maximized.
+//     */
+//    protected function shouldStartMaximized(): bool
+//    {
+//        return isset($_SERVER['DUSK_START_MAXIMIZED']) ||
+//               isset($_ENV['DUSK_START_MAXIMIZED']);
+//    }
+
+
+
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = '0.0.0.0:8889';
+
     /**
      * Prepare for Dusk test execution.
      *
      * @beforeClass
+     * @return void
      */
-    public static function prepare(): void
+    public static function prepare()
     {
-        if (! static::runningInSail()) {
-            static::startChromeDriver();
-        }
+        static::startChromeDriver();
     }
 
     /**
      * Create the RemoteWebDriver instance.
+     *
+     * @return RemoteWebDriver
      */
-    protected function driver(): RemoteWebDriver
+    protected function driver()
     {
-        $options = (new ChromeOptions)->addArguments(collect([
-            $this->shouldStartMaximized() ? '--start-maximized' : '--window-size=1920,1080',
-        ])->unless($this->hasHeadlessDisabled(), function (Collection $items) {
-            return $items->merge([
-                '--disable-gpu',
-                '--headless=new',
-            ]);
-        })->all());
+        $capabilities = DesiredCapabilities::chrome();
+        $options = new ChromeOptions();
+        $options->addArguments([
+            '--disable-gpu',
+            '--headless',
+            '--no-sandbox',
+            '--disable-dev-shm-usage',
+        ]);
+        $capabilities->setCapability(ChromeOptions::CAPABILITY, $options);
 
         return RemoteWebDriver::create(
-            $_ENV['DUSK_DRIVER_URL'] ?? 'http://localhost:9515',
-            DesiredCapabilities::chrome()->setCapability(
-                ChromeOptions::CAPABILITY, $options
-            )
+            'http://selenium:4444/wd/hub',
+            $capabilities
         );
-    }
-
-    /**
-     * Determine whether the Dusk command has disabled headless mode.
-     */
-    protected function hasHeadlessDisabled(): bool
-    {
-        return isset($_SERVER['DUSK_HEADLESS_DISABLED']) ||
-               isset($_ENV['DUSK_HEADLESS_DISABLED']);
-    }
-
-    /**
-     * Determine if the browser window should start maximized.
-     */
-    protected function shouldStartMaximized(): bool
-    {
-        return isset($_SERVER['DUSK_START_MAXIMIZED']) ||
-               isset($_ENV['DUSK_START_MAXIMIZED']);
     }
 }
