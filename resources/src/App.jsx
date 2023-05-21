c3e23c30f4685b799772839bada7263d8d670fb0
pollux83 <pollux83@bk.ru>
2023-05-21 15:30:58 +0300

    mosaic

--- resources/src/App.jsx
+++ resources/src/App.jsx
@@ -1,59 +1,33 @@
-import './bootstrap';
+import React, { useEffect } from 'react';
+import {
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
+
 import './css/style.css';
 import './charts/ChartjsConfig';
 
+// Import pages
 import Dashboard from './pages/Dashboard';
-import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+function App() {
 
-createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
-    setup({ el, App, props }) {
-        const root = createRoot(el);
+  const location = useLocation();
 
-        root.render(<App {...props} />);
-    },
-    progress: {
-        color: '#4B5563',
-    },
-});
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto'
+    window.scroll({ top: 0 })
+    document.querySelector('html').style.scrollBehavior = ''
+  }, [location.pathname]); // triggered on route change
 
+  return (
+    <>
+      <Routes>
+        <Route exact path="/" element={<Dashboard />} />
+      </Routes>
+    </>
+  );
+}
 
-// import React, { useEffect } from 'react';
-// import {
-//   Routes,
-//   Route,
-//   useLocation
-// } from 'react-router-dom';
-//
-// import './css/style.css';
-//
-// import './charts/ChartjsConfig';
-//
-// // Import pages
-// import Dashboard from './pages/Dashboard';
-//
-// function App() {
-//
-//   const location = useLocation();
-//
-//   useEffect(() => {
-//     document.querySelector('html').style.scrollBehavior = 'auto'
-//     window.scroll({ top: 0 })
-//     document.querySelector('html').style.scrollBehavior = ''
-//   }, [location.pathname]); // triggered on route change
-//
-//   return (
-//     <>
-//       <Routes>
-//         <Route exact path="/" element={<Dashboard />} />
-//       </Routes>
-//     </>
-//   );
-// }
-//
-// export default App;
+export default App;
