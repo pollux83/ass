bb0b50be4fe42ea647c4387f6ae75b8df2fe5af8
pollux83 <pollux83@tut.by>
2023-05-23 13:24:58 +0300

    fix minor

--- app/Http/Controllers/ChatGPTController.php
+++ app/Http/Controllers/ChatGPTController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use OpenAI;
 use OpenAI\Client;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
 class ChatGPTController extends Controller
 {
@@ -18,17 +21,31 @@ class ChatGPTController extends Controller
         $this->client = $client;
     }
 
-    public function sendMessage(Request $request, Client $client)
+    public function sendMessage(Request $request = null)
     {
-        $message = $request->input('message');
-        $response = $client->completions()->create([
-            'model' => 'davinci',
-            'prompt' => $message,
+        $message = 'PHP is';
+        //$message = $request->input('message');
+//        $client = OpenAI::factory()
+//            ->withApiKey(getenv('YOUR_API_KEY'))
+//            ->withOrganization(getenv('OPENAI_ORGANIZATION')) // default: null
+//            ->withBaseUri('openai.example.com/v1') // default: api.openai.com/v1
+//            ->withHttpClient($client = new \GuzzleHttp\Client([])) // default: HTTP client found using PSR-18 HTTP Client Discovery
+//            ->withHttpHeader('X-My-Header', 'foo')
+//            ->withQueryParam('my-param', 'bar')
+//            ->withStreamHandler(fn (RequestInterface $request): ResponseInterface => $client->send($request, [
+//                'stream' => true // Allows to provide a custom stream handler for the http client.
+//            ]))
+//            ->make();
+        $response = $this->client->completions()->create([
+            'model' => 'gpt-3.5-turbo',
+            'prompt' => 'PHP is',
             'temperature' => 0.5,
             'max_tokens' => 50,
             'n' => 1,
             'stop' => ['\n'],
         ]);
+        dump($response);
+
         return response()->json([
             'message' => $message,
             'response' => $response->choices[0]->text,
@@ -46,6 +63,14 @@ class ChatGPTController extends Controller
 //            // ...
 //        }
         $list = $response->toArray();
+
+                $this->sendMessage();
+
+
+
+
+
+
         return Inertia::render('ChatGPT/Create', [
             'list' => $list// ['object' => 'list', 'data' => [...]]
         ]);
