8447d69f23ec4824edc9f60ebec431f3ec6bba95
pollux83 <pollux83@bk.ru>
2023-05-24 21:56:07 +0300

    add creation-agent

--- docker/creation-agent/app.py
+++ docker/creation-agent/app.py
@@ -1,33 +1,101 @@
-from flask import Flask, request, jsonify
+import spacy
+from flask import Flask, request
 
 app = Flask(__name__)
+# nlp = spacy.load("en_core_web_sm")
+nlp = spacy.blank("ru")  # Load a blank Russian language model
+
+# Load the pre-trained Russian language model
+# If you have downloaded and installed the model using 'python -m spacy download ru_core_news_sm'
+# you can load it using the following line instead:
+# nlp = spacy.load("ru_core_news_sm")
+
+# Placeholder function for task submission to the task management system or agent
+def submit_task(task_specification):
+    # Add your implementation logic here to submit the task to the appropriate system or agent
+    print("Task submitted:", task_specification)
 
 @app.route('/create_task', methods=['POST'])
 def create_task():
-    data = request.get_json()
+    # 1) Receive instructions or requests from users or external systems
+    instructions = request.form.get('instructions')
 
-    # Perform natural language understanding (NLU) on the input
-    # You can use NLP libraries like spaCy or NLTK for this purpose
+    # 2) Perform Natural Language Understanding (NLU) on the received instructions
+    doc = nlp(instructions)
 
-    # Extract relevant information from the input
-    task_description = data['description']
-    due_date = data['due_date']
-    priority = data['priority']
-    resources = data['resources']
-    dependencies = data['dependencies']
+    # Extract relevant information from the NLU processed instructions
+    # You can add your logic here to handle different NLU tasks (e.g., named entity recognition, part-of-speech tagging)
 
-    # Create a task specification
-    task = {
-        'description': task_description,
-        'due_date': due_date,
-        'priority': priority,
-        'resources': resources,
-        'dependencies': dependencies
+    # Example: Extracting verbs from the instructions
+    verbs = [token.lemma_ for token in doc if token.pos_ == 'VERB']
+
+    # Example: Extracting named entities from the instructions
+    entities = [ent.text for ent in doc.ents]
+
+    # Example: Print the extracted verbs and named entities
+    print("Extracted verbs:", verbs)
+    print("Extracted entities:", entities)
+
+    # Return a response to indicate the successful extraction of information
+    print("NLU processing completed")
+
+
+    # 2) Task Extraction and structuring
+    task_objective = None
+    required_inputs = []
+    constraints = []
+    dependencies = []
+
+    # Example: Extracting the task's objective
+    for sentence in doc.sents:
+        if "task" in sentence.text.lower():
+            task_objective = sentence.text
+            break
+
+    # Example: Extracting required inputs
+    for token in doc:
+        if token.pos_ == 'NOUN':
+            required_inputs.append(token.text)
+
+    # Example: Extracting constraints
+    for token in doc:
+        if token.dep_ == 'aux':
+            constraints.append(token.text)
+
+    # Example: Extracting dependencies
+    for token in doc:
+        if token.dep_ == 'prep':
+            dependencies.append(token.text)
+
+    # Example: Print the extracted task information
+    print("Task objective:", task_objective)
+    print("Required inputs:", required_inputs)
+    print("Constraints:", constraints)
+    print("Dependencies:", dependencies)
+
+    # Return a response to indicate the successful extraction of task information
+    print("Task extraction completed")
+
+    # Task Specification
+    task_specification = {
+        "task_objective": task_objective,
+        "required_inputs": required_inputs,
+        "constraints": constraints,
+        "dependencies": dependencies
+        # Add more fields as needed: due date, priority level, required resources, etc.
     }
 
-    # Optionally, you can store the created task in a database or data structure for further processing
+    # Example: Print the task specification
+    print("Task Specification:", task_specification)
 
-    return jsonify({'message': 'Task created successfully', 'task': task}), 200
+    # Return a response to indicate the successful task specification creation
+    print("Task specification created successfully")
+
+    # Task Submission
+    submit_task(task_specification)
+
+    # Return a response to indicate the successful task submission
+#     return "Task submitted successfully"
 
 if __name__ == '__main__':
     app.run(debug=True)
