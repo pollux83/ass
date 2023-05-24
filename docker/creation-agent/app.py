80c0eab85ce118bf0b0d3089cca3d24928e14b88
pollux83 <pollux83@tut.by>
2023-05-24 16:41:24 +0300

    add creation agent (docker-compose.yml, app.py)

--- docker/creation-agent/app.py
+++ docker/creation-agent/app.py
@@ -1,42 +1,77 @@
-import json
+# import json
+#
+# def receive_instructions():
+#     # Placeholder function for receiving instructions from users or external systems
+#     # You can implement the logic here to receive instructions in any desired format
+#     # For this example, let's assume the instructions are received as a JSON string
+#     instructions = '{"task_name": "Sample Task", "description": "This is a sample task."}'
+#     return json.loads(instructions)
+#
+# def perform_natural_language_understanding(instructions):
+#     # Placeholder function for performing natural language understanding
+#     # You can implement the logic here to extract relevant information from the instructions
+#     # For this example, let's assume the task name and description are the key information
+#     task_name = instructions.get('task_name')
+#     description = instructions.get('description')
+#     return task_name, description
+#
+# def create_task(task_name, description):
+#     # Placeholder function for creating a task
+#     # You can implement the logic here to generate a well-defined task specification
+#     # For this example, let's assume the task is a dictionary with a name and description
+#     task = {'name': task_name, 'description': description}
+#     return task
+#
+# # Main function
+# def main():
+#     # Receive instructions
+#     instructions = receive_instructions()
+#
+#     # Perform natural language understanding
+#     task_name, description = perform_natural_language_understanding(instructions)
+#
+#     # Create task
+#     task = create_task(task_name, description)
+#
+#     # Print the created task (replace this with your desired task handling logic)
+#     print("Created Task:")
+#     print(json.dumps(task, indent=4))
+#
+# # Entry point
+# if __name__ == "__main__":
+#     main()
 
-def receive_instructions():
-    # Placeholder function for receiving instructions from users or external systems
-    # You can implement the logic here to receive instructions in any desired format
-    # For this example, let's assume the instructions are received as a JSON string
-    instructions = '{"task_name": "Sample Task", "description": "This is a sample task."}'
-    return json.loads(instructions)
 
-def perform_natural_language_understanding(instructions):
-    # Placeholder function for performing natural language understanding
-    # You can implement the logic here to extract relevant information from the instructions
-    # For this example, let's assume the task name and description are the key information
-    task_name = instructions.get('task_name')
-    description = instructions.get('description')
-    return task_name, description
+from flask import Flask, request, jsonify
 
-def create_task(task_name, description):
-    # Placeholder function for creating a task
-    # You can implement the logic here to generate a well-defined task specification
-    # For this example, let's assume the task is a dictionary with a name and description
-    task = {'name': task_name, 'description': description}
-    return task
+app = Flask(__name__)
 
-# Main function
-def main():
-    # Receive instructions
-    instructions = receive_instructions()
+@app.route('/create_task', methods=['POST'])
+def create_task():
+    data = request.get_json()
 
-    # Perform natural language understanding
-    task_name, description = perform_natural_language_understanding(instructions)
+    # Perform natural language understanding (NLU) on the input
+    # You can use NLP libraries like spaCy or NLTK for this purpose
 
-    # Create task
-    task = create_task(task_name, description)
+    # Extract relevant information from the input
+    task_description = data['description']
+    due_date = data['due_date']
+    priority = data['priority']
+    resources = data['resources']
+    dependencies = data['dependencies']
 
-    # Print the created task (replace this with your desired task handling logic)
-    print("Created Task:")
-    print(json.dumps(task, indent=4))
+    # Create a task specification
+    task = {
+        'description': task_description,
+        'due_date': due_date,
+        'priority': priority,
+        'resources': resources,
+        'dependencies': dependencies
+    }
 
-# Entry point
-if __name__ == "__main__":
-    main()
+    # Optionally, you can store the created task in a database or data structure for further processing
+
+    return jsonify({'message': 'Task created successfully', 'task': task}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
