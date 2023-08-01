70f428d14b16582e3422b11f04bb759e985c8cec
pollux83 <pollux83@tut.by>
2023-08-01 16:15:07 +0300

    main develop

--- docker/main/main.py
+++ docker/main/main.py
@@ -1,8 +1,16 @@
 import streamlit as st
 
+def generate(text_input):
+    return text_input
+
 def main():
-    st.title("My Streamlit App. Test")
-    st.write("Welcome to my first Streamlit app! Test")
+#     st.title("My Streamlit App. Test")
+#     st.write("Welcome to my first Streamlit app! Test")
+    st.title("Simple LLM-powered App")
+    text_input = st.text_input("Enter your text here:")
+    if st.button("Generate"):
+        response = generate(text_input)
+        st.write(response)
 
 if __name__ == "__main__":
     main()
