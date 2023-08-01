f7a86583a855361b86f6ee1d1020170493832aed
pollux83 <pollux83@tut.by>
2023-08-01 14:17:00 +0300

    streamlit

--- docker/main/main.py
+++ docker/main/main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 def main():
-    st.title("My Streamlit App")
-    st.write("Welcome to my first Streamlit app!")
+    st.title("My Streamlit App1111")
+    st.write("Welcome to my first Streamlit app!1111")
 
 if __name__ == "__main__":
     main()
