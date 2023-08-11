21a617c81de615b1c3d6ace3d101ef54edb7ce60
pollux83 <pollux83@tut.by>
2023-08-11 15:57:57 +0300

    main develop

--- docker/main/alpaca.py
+++ docker/main/alpaca.py
@@ -5,6 +5,10 @@ from langchain import PromptTemplate, LLMChain
 
 import torch
 
+#SETTING UP A CHAT WITH MEMORY
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+
 def generateAlpaca(question = "What is the capital of England?"):
     tokenizer = LlamaTokenizer.from_pretrained("chavinlo/alpaca-native")
     base_model = LlamaForCausalLM.from_pretrained(
@@ -24,7 +28,6 @@ def generateAlpaca(question = "What is the capital of England?"):
     )
     local_llm = HuggingFacePipeline(pipeline=pipe)
 
-    from langchain import PromptTemplate, LLMChain
     template = """Below is an instruction that describes a task. Write a response that appropriately completes the request.
 
     ### Instruction:
@@ -35,6 +38,15 @@ def generateAlpaca(question = "What is the capital of England?"):
 
     llm_chain = LLMChain(prompt=prompt,llm=local_llm)
     return llm_chain.run(question)
+
+    # We are going to set the memory to go back 4 turns
+    window_memory = ConversationBufferWindowMemory(k=4)
+
+    conversation = ConversationChain(
+        llm=local_llm,
+        verbose=True,
+        memory=window_memory
+    )
 
 
 
